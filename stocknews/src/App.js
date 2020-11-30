d492424951080a1e0b5f7a8529677dae0c715784
unknown <justingrant99@me.com>
2020-11-29 17:11:43 -0800

    fetched news

--- stocknews/src/App.js
+++ stocknews/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username:'',
+      news: []
+    }
+  }
+  newsCreator = (array) =>{
+    return(
+      <div>{array['summary']}</div>
+    )
+   
+  }
+
+
+  fetchStockNews = (props) => {
+    const pointerToThis = this;
+    var tempNewsArray = []
+    const API_NEWS_KEY = 'dSgyySKSrTp44mgcX443KL8bxVv5vWMg'
+    let API_CALLS = 'https://api.polygon.io/v1/meta/symbols/'+'IBM'+'/news?perpage=5&page=1&apiKey=' + API_NEWS_KEY
+    fetch(API_CALLS)
+    .then(response => response.json())
+     .then(
+      data => {
+      console.log(data);
+      for(var index in data){
+        let newsArticle = {title: data[index]['title'], 
+        summary: data[index]['summary'], url: data[index]['url'], image: data[index]['image']}
+       
+      tempNewsArray.push(newsArticle)
+      }
+      this.setState({
+        news: tempNewsArray
+      })
+  
+       }
+     )
+      }
+
+  render(){
+    return (
+   <div>
+     <button onClick = {this.fetchStockNews}>fetch</button>
+    <div>{this.state.news.map(this.newsCreator)}</div>
+   </div>
+      );
+
+  }
+  
 }
 
 export default App;
